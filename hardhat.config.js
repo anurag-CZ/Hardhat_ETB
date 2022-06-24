d93673f242186fde74dd70063acdf768d3ef52a5
Anurag Chemban <anurag.chemban@codezeros.com>
2022-06-24 18:41:14 +0530

    config

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,9 +4,9 @@ require("@nomiclabs/hardhat-waffle");
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-const INFURA_URI_ROPSTEN = "https://ropsten.infura.io/v3/6ac45c570c8f4d3cb1ea891b7b0e3ecc";
-const INFURA_URI_RINKEBY = 'https://rinkeby.infura.io/v3/6ac45c570c8f4d3cb1ea891b7b0e3ecc';
-const PRIVATE_KEY = "b191b1348b71e4a55425eefd7cef1e69a2a18456cabca4a12af77d396986bcf8"; 
+const INFURA_URI_ROPSTEN = "";
+const INFURA_URI_RINKEBY = '';
+const PRIVATE_KEY = ""; 
 
 
 module.exports = {
